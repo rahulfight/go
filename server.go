4a5e489faf61e56ca0f31ef3a582cd989589aff8
rahulfight <53572523+rahulfight@users.noreply.github.com>
2019-12-27 13:53:52 -0500

    initial

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Hello World %s!", request.URL.Path[1:])
+}
+
+func handlerTest(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "This is test %s!", request.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/test", handlerTest)
+	http.ListenAndServe(":8080", nil)
+}
